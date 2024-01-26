770c6cfc073d827955ddf13cbff19c6cc5a98691
Mikko Kaivola <mkaivola96@gmail.com>
2024-01-26 14:38:31 +0200

    PCA Analysis

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 import os
+
+
 
 # Read train and test data
 vowel_train, vowel_test = pandas.read_csv("vowel/vowel_train.csv"), \
@@ -29,3 +32,37 @@ label_fig.savefig("vowel/label_hist.pdf")
 
 # Label distribution is uniform
 
+# Analyze label distributions via PCA
+
+vowel_train_features = vowel_train.drop('y', axis = 1)
+vowel_train_labels = vowel_train['y']
+
+vowel_PCA = PCA()
+vowel_PCA.fit(vowel_train_features)
+
+# Plot the cumulative explained variance as a barplot
+
+explained_vars_cum = np.array(vowel_PCA.explained_variance_ratio_).cumsum()
+
+var_bar_fig, var_bar_ax = plt.subplots()
+
+var_bar_ax.bar(np.arange(1, vowel_PCA.n_components_ + 1 ), explained_vars_cum)
+var_bar_ax.set_xlabel("Principal component")
+var_bar_ax.set_ylabel("Prop. of variance accounted")
+var_bar_ax.set_title("Distribution of variance over PCs")
+
+var_bar_ax.axhline(0.9, ls = '--', c = 'red')
+
+var_bar_fig.savefig("vowel/PCA_var_dist.pdf")
+
+# Plot 2D rep
+vowel_feat_PCA_projec = vowel_PCA.transform(vowel_train_features)
+
+pca_fig, pca_ax = plt.subplots()
+
+pca_ax.scatter(vowel_feat_PCA_projec[:,0], vowel_feat_PCA_projec[:,1], c = vowel_train_labels)
+pca_ax.set_ylabel("PC 2")
+pca_ax.set_xlabel("PC 1")
+pca_ax.set_title(f'Feature projection via PCA, Prop. of total var: {explained_vars_cum[1].round(2)} ')
+
+pca_fig.savefig("vowel/PC_2D.pdf")
