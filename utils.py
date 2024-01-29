2de4774d69f2bdcfecde88e14dd95815c0385f57
Mikko Kaivola <mkaivola96@gmail.com>
2024-01-29 18:35:45 +0200

    OvR Micro-Avg ROC curve func

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import RocCurveDisplay
 
 def heat_map_plot(data_array: np.ndarray, xlabels: list, ylabels: list, filename: str):
     """
@@ -41,3 +43,43 @@ def heat_map_plot(data_array: np.ndarray, xlabels: list, ylabels: list, filename
 
     fig.tight_layout()
     fig.savefig(filename)
+
+def roc_multiclass_plot(true_labels_train: np.ndarray, true_labels_test: np.ndarray, 
+                        predict_scores_test: np.ndarray, method: str,
+                        filename: str):
+    """ 
+    Plot a micro-averaged One-vs-Rest (OvR) ROC curve and save the figure to file system
+    
+    Arguments
+    ---------
+    true_labels_train
+        A 2D array containing the true labels of the training set samples
+    true_labels_test
+        A 2D array containing the true labels of the test set samples
+    predict_scores_test
+        A 2D array containing the predicted scores of the test set samples
+    method:
+        A string containing the name of the classification method
+    filename:
+        A string specifying the path to save the figure
+
+    """
+
+    # Binarize the multiclass labels
+    binarizer = LabelBinarizer().fit(true_labels_train)
+    binarized_labels_test = binarizer.transform(true_labels_test)
+
+    # Compute the micro-averaged OvR ROC curve
+
+    roc_plot = RocCurveDisplay.from_predictions(y_true = binarized_labels_test.ravel(),
+                                                y_pred = predict_scores_test.ravel(),
+                                                name = "Micro-averaged OvR",
+                                                plot_chance_level = True,
+                                                color = 'blue')
+    roc_plot.ax_.set(
+        xlabel = "False positive rate",
+        ylabel = "True positive rate",
+        title = f"Method: {method}"
+    )
+
+    roc_plot.figure_.savefig(filename)
