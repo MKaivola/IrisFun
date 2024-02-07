5dd642764cc7ee1203b610b8a01146a80dd9c207
Mikko Kaivola <mkaivola96@gmail.com>
2024-02-07 19:20:15 +0200

    Training script for a classifier
    - A script that has CLI for specifying training data
    - Accuracy evaluated via cross-validation
    - Persists the model via skops
    
    - TODO: Input validation
    - TODO: Handling separate test data

---
+++ train_model.py
@@ -0,0 +1,47 @@
+import argparse
+
+import pandas
+import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, cross_val_score
+import skops.io as sio
+from scipy.stats import uniform
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("filename_data",
+        type=str, 
+        help="Path to a csv file containing the training data")
+parser.add_argument("--filename_test",
+        type=str, 
+        help="Path to a csv file containing the test data")
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    train_data = pandas.read_csv(args.filename_data)
+
+    # TODO: Input validation
+
+    X = train_data.drop(['row.names', 'y'], axis = 1)
+    y = train_data['y']
+
+    model_pipe = Pipeline([("scaler", StandardScaler()), ("LDA", LinearDiscriminantAnalysis(solver = 'eigen'))])
+
+    hyper_param_grid = {
+        "LDA__shrinkage": np.concatenate(([0], uniform.rvs(size = 15, random_state = 0), [1]))
+    }
+
+    # Estimate generalization error via nested cross-validation
+    model_cv = GridSearchCV(model_pipe, hyper_param_grid)
+
+    model_performance = cross_val_score(model_cv, X, y)
+    print(f"Cross-validated accuracy score: {np.round(model_performance.mean(), 2)}")
+
+    # Learn model using all data
+    model_cv.fit(X, y)
+
+    # Save final model
+    sio.dump(model_cv, "vowel_model.skops")
