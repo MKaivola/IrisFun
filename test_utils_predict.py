d7d07f7a65731c0ca53def87818608c4793372e2
Mikko Kaivola <mkaivola96@gmail.com>
2024-03-21 19:35:16 +0200

    Input validation for prediction
    - Implemented an input validation function for model prediction script
    - Implemented unit tests for this function
    - Implemented a simple preprocessing function for prediction
    - Added an attribute to the classifier custom class

--- test_utils_predict.py
+++ test_utils_predict.py
@@ -1,6 +1,8 @@
 import pytest
 from sklearn.dummy import DummyClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import pandas as pd
+import numpy as np
 import skops.io as sio
 
 import utils_predict
@@ -36,3 +38,48 @@ class TestLoadModel():
         with pytest.raises(KeyboardInterrupt):
             monkeypatch.setattr("builtins.input", lambda _: 'n')
             utils_predict.load_model(full_path.as_posix())
+
+@pytest.fixture
+def unlabeled_df():
+
+    n = 10
+
+    df = pd.DataFrame(np.random.rand(n,2))
+
+    df = df.rename(columns = {0:'row.names'})
+
+    return df
+
+@pytest.fixture
+def dummy_model(unlabeled_df):
+    model = LinearDiscriminantAnalysis()
+
+    labels = np.random.randint(low = 0, high = 3, size = unlabeled_df.shape[0])
+
+    model.fit(unlabeled_df.drop('row.names', axis = 1), labels)
+
+    return model
+
+
+class TestValidateInput():
+
+    def test_missing_row_names(self, unlabeled_df, dummy_model):
+
+        df = unlabeled_df.drop('row.names', axis = 1)
+
+        with pytest.raises(KeyError):
+            utils_predict.validate_input(df, dummy_model)
+
+    def test_non_numeric_column(self, unlabeled_df, dummy_model):
+
+        df = unlabeled_df.assign(text = np.repeat('foo', unlabeled_df.shape[0]))
+
+        with pytest.raises(TypeError):
+            utils_predict.validate_input(df, dummy_model)
+
+    def test_different_amounts_of_features(self, unlabeled_df, dummy_model):
+
+        df = unlabeled_df.assign(new_feature = np.repeat(0.0, unlabeled_df.shape[0]))
+
+        with pytest.raises(ValueError):
+            utils_predict.validate_input(df, dummy_model)
--- utils_predict.py
+++ utils_predict.py
@@ -4,6 +4,7 @@ import pandas as pd
 import skops.io as sio
 
 import custom_types
+from utils import df_col_type_check
 
 def load_model(filepath: str) -> custom_types.Scikit_Classifier:
     """
@@ -37,3 +38,35 @@ def load_model(filepath: str) -> custom_types.Scikit_Classifier:
             raise KeyboardInterrupt("The model file was not loaded, program interrupted")
         
     sio.load(filepath, trusted = unknown_types)
+
+def validate_input(data_matrix: pd.DataFrame, model: custom_types.Scikit_Classifier) -> None:
+    """
+    Check that the input data frame conforms to the preprocessing assumptions
+    Additionally, check that the number of features are the same between the data and the model
+
+    Arguments
+    ---------
+    input_data
+        A pandas dataframe containing new unlabeled data
+    model
+        A learned sklearn classifier
+    """
+
+    col_names = data_matrix.columns
+
+    if 'row.names' not in col_names:
+        raise KeyError("'row.names' column is missing from the dataframe")
+    
+    df_col_type_check(data_matrix.drop('row.names', axis = 1))
+
+    model_features = model.n_features_in_
+    data_features = data_matrix.shape[-1] - 1
+
+    if data_features != model_features:
+        raise ValueError(f"The number of features in the model and the data are not equal. Expected {model_features}, got {data_features} instead.")
+    
+def preprocess_input(data_matrix: pd.DataFrame) -> pd.DataFrame:
+
+    X = data_matrix.drop(['row.names'], axis = 1)
+
+    return X
